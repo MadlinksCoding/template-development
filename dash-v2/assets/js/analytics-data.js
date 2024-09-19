f36a8d1c7a9bfb17396961c174c456fc1f2ecd21
prosenjitsynfosolutions <prosenjitd.wt@gmail.com>
2024-09-19 18:01:54 +0530

    up

---
+++ dash-v2/assets/js/analytics-data.js
@@ -0,0 +1,577 @@
+// all charts data set
+var chartDataSet = {
+   salesInsightBarData: [{
+       "month": "Jan",
+       "year": 2023,
+       "subscription": 150,
+       "paytoview": 90,
+       "merch": 50,
+       "wishtender": 70,
+       "customrequest": 20
+   },
+   {
+       "month": "Feb",
+       "year": 2023,
+       "subscription": 130,
+       "paytoview": 100,
+       "merch": 60,
+       "wishtender": 80,
+       "customrequest": 50
+   },
+   {
+       "month": "Mar",
+       "year": 2023,
+       "subscription": 150,
+       "paytoview": 100,
+       "merch": 150,
+       "wishtender": 70,
+       "customrequest": 20
+   },
+   {
+       "month": "Apr",
+       "year": 2023,
+       "subscription": 150,
+       "paytoview": 160,
+       "merch": 100,
+       "wishtender": 100,
+       "customrequest": 50
+   },
+   {
+       "month": "May",
+       "year": 2023,
+       "subscription": 180,
+       "paytoview": 180,
+       "merch": 100,
+       "wishtender": 100,
+       "customrequest": 50
+   },
+   {
+       "month": "Jun",
+       "year": 2023,
+       "subscription": 200,
+       "paytoview": 200,
+       "merch": 100,
+       "wishtender": 100,
+       "customrequest": 50
+   },
+   {
+       "month": "Jul",
+       "year": 2023,
+       "subscription": 250,
+       "paytoview": 200,
+       "merch": 100,
+       "wishtender": 100,
+       "customrequest": 50
+   },
+   {
+       "month": "Aug",
+       "year": 2023,
+       "subscription": 300,
+       "paytoview": 200,
+       "merch": 100,
+       "wishtender": 100,
+       "customrequest": 50
+   },
+   {
+       "month": "Sep",
+       "year": 2023,
+       "subscription": 350,
+       "paytoview": 200,
+       "merch": 100,
+       "wishtender": 100,
+       "customrequest": 50
+   },
+   {
+       "month": "Oct",
+       "year": 2023,
+       "subscription": 380,
+       "paytoview": 200,
+       "merch": 100,
+       "wishtender": 100,
+       "customrequest": 50
+   },
+   {
+       "month": "Nov",
+       "year": 2023,
+       "subscription": 400,
+       "paytoview": 200,
+       "merch": 100,
+       "wishtender": 100,
+       "customrequest": 50
+   },
+   {
+       "month": "Dec",
+       "year": 2023,
+       "subscription": 500,
+       "paytoview": 200,
+       "merch": 100,
+       "wishtender": 100,
+       "customrequest": 50
+   }],
+   salesInsightLineData: [{
+           month: "Jan",
+           fullMonth: "JANUARY",
+           year: 2023,
+           subscription: 155,
+           paytoview: 120,
+           merch: 90,
+           wishtender: 120,
+           customrequest: 50
+       },
+       {
+           month: "Feb",
+           fullMonth: "FEBRUARY",
+           year: 2023,
+           subscription: 105,
+           paytoview: 95,
+           merch: 145,
+           wishtender: 120,
+           customrequest: 50
+       },
+       {
+           month: "Mar",
+           fullMonth: "MARCH",
+           year: 2023,
+           subscription: 110,
+           paytoview: 130,
+           merch: 125,
+           wishtender: 110,
+           customrequest: 100
+       },
+       {
+           month: "Apr",
+           fullMonth: "APRIL",
+           year: 2023,
+           subscription: 115,
+           paytoview: 165,
+           merch: 105,
+           wishtender: 160,
+           customrequest: 90
+       },
+       {
+           month: "May",
+           fullMonth: "MAY",
+           year: 2023,
+           subscription: 120,
+           paytoview: 160,
+           merch: 140,
+           wishtender: 120,
+           customrequest: 90
+       },
+       {
+           month: "Jun",
+           fullMonth: "JUNE",
+           year: 2023,
+           subscription: 125,
+           paytoview: 145,
+           merch: 165,
+           wishtender: 140,
+           customrequest: 90
+       },
+       {
+           month: "Jul",
+           fullMonth: "JULY",
+           year: 2023,
+           subscription: 150,
+           paytoview: 130,
+           merch: 170,
+           wishtender: 120,
+           customrequest: 170
+       },
+       {
+           month: "Aug",
+           fullMonth: "AUGUST",
+           year: 2023,
+           subscription: 140,
+           paytoview: 150,
+           merch: 135,
+           wishtender: 120,
+           customrequest: 90
+       },
+       {
+           month: "Sep",
+           fullMonth: "SEPTEMBER",
+           year: 2023,
+           subscription: 130,
+           paytoview: 110,
+           merch: 120,
+           wishtender: 180,
+           customrequest: 120
+       },
+       {
+           month: "Oct",
+           fullMonth: "OCTOBER",
+           year: 2023,
+           subscription: 160,
+           paytoview: 170,
+           merch: 150,
+           wishtender: 120,
+           customrequest: 90
+       },
+       {
+           month: "Nov",
+           fullMonth: "NOVEMBER",
+           year: 2023,
+           subscription: 170,
+           paytoview: 180,
+           merch: 155,
+           wishtender: 140,
+           customrequest: 150
+       },
+       {
+           month: "Dec",
+           fullMonth: "DECEMBER",
+           year: 2023,
+           subscription: 180,
+           paytoview: 190,
+           merch: 160,
+           wishtender: 120,
+           customrequest: 90
+   }],
+   tokenInsightBarData: [{
+           "month": "Jan",
+           "year": 2023,
+           "tip": 150,
+           "call": 90,
+           "chat": 50,
+           "livestreaming": 70,
+       }, {
+           "month": "Feb",
+           "year": 2023,
+           "tip": 250,
+           "call": 190,
+           "chat": 150,
+           "livestreaming": 170,
+       }, {
+           "month": "Mar",
+           "year": 2023,
+           "tip": 350,
+           "call": 210,
+           "chat": 200,
+           "livestreaming": 170,
+       }, {
+           "month": "Apr",
+           "year": 2023,
+           "tip": 250,
+           "call": 190,
+           "chat": 150,
+           "livestreaming": 170,
+       }, {
+           "month": "May",
+           "year": 2023,
+           "tip": 350,
+           "call": 210,
+           "chat": 200,
+           "livestreaming": 170,
+       }, {
+           "month": "Jun",
+           "year": 2023,
+           "tip": 250,
+           "call": 250,
+           "chat": 150,
+           "livestreaming": 170,
+       }, {
+           "month": "Jul",
+           "year": 2023,
+           "tip": 350,
+           "call": 290,
+           "chat": 250,
+           "livestreaming": 170,
+       }, {
+           "month": "Aug",
+           "year": 2023,
+           "tip": 350,
+           "call": 210,
+           "chat": 200,
+           "livestreaming": 170,
+       }, {
+           "month": "Sep",
+           "year": 2023,
+           "tip": 350,
+           "call": 210,
+           "chat": 200,
+           "livestreaming": 170,
+       }, {
+           "month": "Oct",
+           "year": 2023,
+           "tip": 350,
+           "call": 210,
+           "chat": 200,
+           "livestreaming": 170,
+       }, {
+           "month": "Nov",
+           "year": 2023,
+           "tip": 350,
+           "call": 210,
+           "chat": 200,
+           "livestreaming": 170,
+       }, {
+           "month": "Dec",
+           "year": 2023,
+           "tip": 250,
+           "call": 210,
+           "chat": 200,
+           "livestreaming": 270,
+   }],
+   tokenInsightLineData: [{
+           month: "Jan",
+           fullMonth: "JANUARY",
+           year: 2023,
+           tip: 170,
+           call: 130,
+           chat: 50,
+           livestreaming: 40
+       },
+       {
+           month: "Feb",
+           fullMonth: "FEBRUARY",
+           year: 2023,
+           tip: 50,
+           call: 120,
+           chat: 90,
+           livestreaming: 50
+       },
+       {
+           month: "Mar",
+           fullMonth: "MARCH",
+           year: 2023,
+           tip: 300,
+           call: 120,
+           chat: 120,
+           livestreaming: 250
+       },
+       {
+           month: "Apr",
+           fullMonth: "APRIL",
+           year: 2023,
+           tip: 400,
+           call: 520,
+           chat: 160,
+           livestreaming: 156
+       },
+       {
+           month: "May",
+           fullMonth: "MAY",
+           year: 2023,
+           tip: 155,
+           call: 220,
+           chat: 170,
+           livestreaming: 521
+       },
+       {
+           month: "Jun",
+           fullMonth: "JUNE",
+           year: 2023,
+           tip: 100,
+           call: 320,
+           chat: 180,
+           livestreaming: 124
+       },
+       {
+           month: "Jul",
+           fullMonth: "JULY",
+           year: 2023,
+           tip: 250,
+           call: 120,
+           chat: 150,
+           livestreaming: 50
+       },
+       {
+           month: "Aug",
+           fullMonth: "AUGUST",
+           year: 2023,
+           tip: 655,
+           call: 420,
+           chat: 200,
+           livestreaming: 52
+       },
+       {
+           month: "Sep",
+           fullMonth: "SEPTEMBER",
+           year: 2023,
+           tip: 750,
+           call: 820,
+           chat: 400,
+           livestreaming: 158
+       },
+       {
+           month: "Oct",
+           fullMonth: "OCTOBER",
+           year: 2023,
+           tip: 160,
+           call: 100,
+           chat: 140,
+           livestreaming: 320
+       },
+       {
+           month: "Nov",
+           fullMonth: "NOVEMBER",
+           year: 2023,
+           tip: 255,
+           call: 120,
+           chat: 150,
+           livestreaming: 485
+       },
+       {
+           month: "Dec",
+           fullMonth: "DECEMBER",
+           year: 2023,
+           tip: 50,
+           call: 60,
+           chat: 90,
+           livestreaming: 120
+   }],
+   locationMapChartData: [{
+       "name": "Hong Kong",
+       "color": "#3A0CA4",
+       "data": [{
+           "id": "HK",
+           "sales": "5432"
+       }, ]
+   }, {
+       "name": "United States of America",
+       "color": "#3F37C9",
+       "data": [{
+           "id": "US",
+           "sales": "4432"
+       }, ]
+   }, {
+       "name": "Singapore",
+       "color": "#425CE9",
+       "data": [{
+           "id": "SG",
+           "sales": "3432"
+       }, ]
+   }, {
+       "name": "Taiwan",
+       "color": "#814DCA",
+       "data": [{
+           "id": "TW",
+           "sales": "2432"
+       }]
+   }, {
+       "name": "Australia",
+       "color": "#EE278A",
+       "data": [{
+           "id": "AU",
+           "sales": "1432"
+       }]
+   }, {
+       "name": "China",
+       "color": "#C734A1",
+       "data": [{
+           "id": "CN",
+           "sales": "932"
+       }, ]
+   }, {
+       "name": "Russia",
+       "color": "#A653EF",
+       "data": [{
+           "id": "RU",
+           "sales": "832"
+       }, ]
+   }, {
+       "name": "Greenland",
+       "color": "#69A4F0",
+       "data": [{
+           "id": "GL",
+           "sales": "732"
+       }, ]
+   }, {
+       "name": "Canada",
+       "color": "#6CCCEC",
+       "data": [{
+           "id": "CA",
+           "sales": "632"
+       }]
+   }, {
+       "name": "Brazil",
+       "color": "#92D0E6",
+       "data": [{
+           "id": "BR",
+           "sales": "532"
+       }]
+      }],
+   MonthlyBarChart: [
+         { day: "Jul 1", year: 2023, subscription: 155, paytoview: 120, merch: 90, wishtender: 120, customrequest: 50 },
+         { day: "Jul 2", year: 2023, subscription: 105, paytoview: 95, merch: 145, wishtender: 120, customrequest: 50 },
+         { day: "Jul 3", year: 2023, subscription: 110, paytoview: 130, merch: 125, wishtender: 110, customrequest: 100 },
+         { day: "Jul 4", year: 2023, subscription: 115, paytoview: 165, merch: 105, wishtender: 160, customrequest: 90 },
+         { day: "Jul 5", year: 2023, subscription: 120, paytoview: 160, merch: 140, wishtender: 120, customrequest: 90 },
+         { day: "Jul 6", year: 2023, subscription: 125, paytoview: 145, merch: 165, wishtender: 140, customrequest: 90 },
+         { day: "Jul 7", year: 2023, subscription: 150, paytoview: 130, merch: 170, wishtender: 120, customrequest: 170 },
+         { day: "Jul 8", year: 2023, subscription: 140, paytoview: 150, merch: 135, wishtender: 120, customrequest: 90 },
+         { day: "Jul 9", year: 2023, subscription: 130, paytoview: 110, merch: 120, wishtender: 180, customrequest: 120 },
+         { day: "Jul 10", year: 2023, subscription: 160, paytoview: 170, merch: 150, wishtender: 120, customrequest: 90 },
+         { day: "Jul 11", year: 2023, subscription: 170, paytoview: 180, merch: 155, wishtender: 140, customrequest: 150 },
+         { day: "Jul 12", year: 2023, subscription: 180, paytoview: 190, merch: 160, wishtender: 120, customrequest: 90 },
+         { day: "Jul 13", year: 2023, subscription: 160, paytoview: 170, merch: 150, wishtender: 120, customrequest: 90 },
+         { day: "Jul 14", year: 2023, subscription: 170, paytoview: 180, merch: 155, wishtender: 140, customrequest: 150 },
+         { day: "Jul 15", year: 2023, subscription: 180, paytoview: 190, merch: 160, wishtender: 120, customrequest: 90 },
+         { day: "Jul 16", year: 2023, subscription: 160, paytoview: 170, merch: 150, wishtender: 120, customrequest: 90 },
+         { day: "Jul 17", year: 2023, subscription: 170, paytoview: 180, merch: 155, wishtender: 140, customrequest: 150 },
+         { day: "Jul 18", year: 2023, subscription: 180, paytoview: 190, merch: 160, wishtender: 120, customrequest: 90 },
+         { day: "Jul 19", year: 2023, subscription: 160, paytoview: 170, merch: 150, wishtender: 120, customrequest: 90 },
+         { day: "Jul 20", year: 2023, subscription: 170, paytoview: 180, merch: 155, wishtender: 140, customrequest: 150 },
+         { day: "Jul 21", year: 2023, subscription: 180, paytoview: 190, merch: 160, wishtender: 120, customrequest: 90 },
+         { day: "Jul 22", year: 2023, subscription: 160, paytoview: 170, merch: 150, wishtender: 120, customrequest: 90 },
+         { day: "Jul 23", year: 2023, subscription: 170, paytoview: 180, merch: 155, wishtender: 140, customrequest: 150 },
+         { day: "Jul 24", year: 2023, subscription: 180, paytoview: 190, merch: 160, wishtender: 120, customrequest: 90 },
+         { day: "Jul 25", year: 2023, subscription: 160, paytoview: 170, merch: 150, wishtender: 120, customrequest: 90 },
+         { day: "Jul 26", year: 2023, subscription: 170, paytoview: 180, merch: 155, wishtender: 140, customrequest: 150 },
+         { day: "Jul 27", year: 2023, subscription: 180, paytoview: 190, merch: 160, wishtender: 120, customrequest: 90 },
+         { day: "Jul 28", year: 2023, subscription: 160, paytoview: 170, merch: 150, wishtender: 120, customrequest: 90 },
+         { day: "Jul 29", year: 2023, subscription: 170, paytoview: 180, merch: 155, wishtender: 140, customrequest: 150 },
+         { day: "Jul 30", year: 2023, subscription: 180, paytoview: 190, merch: 160, wishtender: 120, customrequest: 90 }
+   ],
+   MonthlyLineChart: [
+      { day: "Jul 1", year: 2023, tip: 170, call: 130, chat: 50, livestreaming: 40},
+      { day: "Jul 2", year: 2023, tip: 50, call: 120, chat: 90, livestreaming: 50 },
+      { day: "Jul 3", year: 2023, tip: 300, call: 120, chat: 120, livestreaming: 250 },
+      { day: "Jul 4", year: 2023, tip: 400, call: 520, chat: 160, livestreaming: 156 },
+      { day: "Jul 5", year: 2023, tip: 155, call: 220, chat: 170, livestreaming: 521 },
+      { day: "Jul 6", year: 2023, tip: 100, call: 320, chat: 180, livestreaming: 124 },
+      { day: "Jul 7", year: 2023, tip: 250, call: 120, chat: 150, livestreaming: 50 },
+      { day: "Jul 8", year: 2023, tip: 655, call: 420, chat: 200, livestreaming: 52 },
+      { day: "Jul 9", year: 2023, tip: 750, call: 820, chat: 400, livestreaming: 158 },
+      { day: "Jul 10", year: 2023, tip: 160, call: 100, chat: 140, livestreaming: 320 },
+      { day: "Jul 11", year: 2023, tip: 170, call: 130, chat: 50, livestreaming: 40},
+      { day: "Jul 12", year: 2023, tip: 50, call: 120, chat: 90, livestreaming: 50 },
+      { day: "Jul 13", year: 2023, tip: 300, call: 120, chat: 120, livestreaming: 250 },
+      { day: "Jul 14", year: 2023, tip: 400, call: 520, chat: 160, livestreaming: 156 },
+      { day: "Jul 15", year: 2023, tip: 155, call: 220, chat: 170, livestreaming: 521 },
+      { day: "Jul 16", year: 2023, tip: 100, call: 320, chat: 180, livestreaming: 124 },
+      { day: "Jul 17", year: 2023, tip: 250, call: 120, chat: 150, livestreaming: 50 },
+      { day: "Jul 18", year: 2023, tip: 655, call: 420, chat: 200, livestreaming: 52 },
+      { day: "Jul 19", year: 2023, tip: 750, call: 820, chat: 400, livestreaming: 158 },
+      { day: "Jul 20", year: 2023, tip: 160, call: 100, chat: 140, livestreaming: 320 },
+      { day: "Jul 21", year: 2023, tip: 170, call: 130, chat: 50, livestreaming: 40},
+      { day: "Jul 22", year: 2023, tip: 50, call: 120, chat: 90, livestreaming: 50 },
+      { day: "Jul 23", year: 2023, tip: 300, call: 120, chat: 120, livestreaming: 250 },
+      { day: "Jul 24", year: 2023, tip: 400, call: 520, chat: 160, livestreaming: 156 },
+      { day: "Jul 25", year: 2023, tip: 155, call: 220, chat: 170, livestreaming: 521 },
+      { day: "Jul 26", year: 2023, tip: 100, call: 320, chat: 180, livestreaming: 124 },
+      { day: "Jul 27", year: 2023, tip: 250, call: 120, chat: 150, livestreaming: 50 },
+      { day: "Jul 28", year: 2023, tip: 655, call: 420, chat: 200, livestreaming: 52 },
+      { day: "Jul 29", year: 2023, tip: 750, call: 820, chat: 400, livestreaming: 158 },
+      { day: "Jul 30", year: 2023, tip: 160, call: 100, chat: 140, livestreaming: 320 }
+   ],
+   WeeklyBarChart: [
+      { "day": "Jul 1", "subscription": 200, "paytoview": 80, "merch": 50, "wishtender": 30, "customrequest": 30 },
+      { "day": "Jul 2", "subscription": 220, "paytoview": 120, "merch": 90, "wishtender": 50, "customrequest": 60 },
+      { "day": "Jul 3", "subscription": 280, "paytoview": 160, "merch": 120, "wishtender": 140, "customrequest": 80 },
+      { "day": "Jul 4", "subscription": 280, "paytoview": 160, "merch": 120, "wishtender": 140, "customrequest": 80 },
+      { "day": "Jul 5","subscription": 210, "paytoview": 160, "merch": 120, "wishtender": 140, "customrequest": 80 },
+      { "day": "Jul 6", "subscription": 280, "paytoview": 160, "merch": 120, "wishtender": 100, "customrequest": 80 },
+      { "day": "Jul 7", "subscription": 80, "paytoview": 160, "merch": 120, "wishtender": 140, "customrequest": 80 }
+   ],
+   WeeklylineChart: [
+      { day: "Jul 1", year: 2023, tip: 170, call: 130, chat: 50, livestreaming: 40},
+      { day: "Jul 2", year: 2023, tip: 50, call: 120, chat: 90, livestreaming: 50 },
+      { day: "Jul 3", year: 2023, tip: 300, call: 120, chat: 120, livestreaming: 250 },
+      { day: "Jul 4", year: 2023, tip: 400, call: 520, chat: 160, livestreaming: 156 },
+      { day: "Jul 5", year: 2023, tip: 155, call: 220, chat: 170, livestreaming: 521 },
+      { day: "Jul 6", year: 2023, tip: 100, call: 320, chat: 180, livestreaming: 124 },
+      { day: "Jul 7", year: 2023, tip: 250, call: 120, chat: 150, livestreaming: 50 }
+    ],
+};
