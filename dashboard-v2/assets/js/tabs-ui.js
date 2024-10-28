c33df3df7b27f736e766256a110c492f0147f097
Abir Maiti <abirlal@capitalnumbers.com>
2024-10-28 13:33:51 +0530

    minor js update

--- dashboard-v2/assets/js/tabs-ui.js
+++ dashboard-v2/assets/js/tabs-ui.js
@@ -1,5 +1,126 @@
 /**
  * 
+ * @class DashTabs
+ * @author Abirlal Maiti <abirlal.maiti@gmail.com>
+ * @classdesc Fansocial Dashboard Profile Tabs UI behavior and handlers (Alternative of the old one defined in pageElements.js)
+ * Clicking on Tab triggers under tab head will make the corresponding tab content container active and throw a custom event with details
+ * 
+ */
+class DashTabs {
+    constructor(element) {
+        this.element = element; // the main tab wrapper element
+        this.tabTriggers = element.querySelectorAll('[data-tab-trigger]'); // the tab trigger elements
+        this.tabContents = element.querySelectorAll('[data-tabbed-content]'); // the tab content containers (id'd)
+
+        // attach event handler (click) to the triggers
+        this.tabTriggers.forEach((trigger) => {
+            trigger.addEventListener('click', this.handleTabTriggerClick.bind(this));
+        });
+    }
+
+    /** 
+     * 
+     * @method handleTabTriggerClick 
+     * @description Handles click event on tab triggers
+     * 
+     */
+    handleTabTriggerClick(event) {
+        const trigger = event.target; // current clicked trigger element
+
+        // detect target tab content container ID and the target content container element
+        const targetTabContentId = trigger.closest('[data-tab-head-list-item]').getAttribute('data-tabbed-child-id');
+        const targetTabContent = this.element.querySelector(`[data-tabbed-content="${targetTabContentId}"]`);
+
+        // set current clicked trigger as active
+        this.updateTabTriggers(trigger);
+
+        // set current target content container as active
+        this.updateTabContents(targetTabContent);
+
+        // get payload from trigger's parent 'li[data-tab-head-list-item]' element
+        const payload = this.getPayload(trigger);
+
+        // get event name from trigger's data-* attribute
+        const eventName = trigger.getAttribute('data-throw-event');
+
+        // invoke custom event
+        this.throwEvent(eventName, payload);
+    }
+
+    /** 
+     * 
+     * @method getPayload 
+     * @description Creates and returns data attributes of parent element of trigger as JSON payload
+     * 
+     */
+    getPayload(trigger) {
+        // get current trigger element's parent 'li' element
+        const tabHeadListItem = trigger.closest('[data-tab-head-list-item]');
+        const payload = {};
+
+        // loop through the parent elements data-* attributes and add them as JSON (can use element.dataset as well for camelCase key)
+        tabHeadListItem.getAttributeNames().forEach((attribute) => {
+            if (attribute.startsWith('data-')) {
+                payload[attribute.replace('data-', '')] = tabHeadListItem.getAttribute(attribute);
+            }
+        });
+
+        return payload;
+    }
+
+    /** 
+     * 
+     * @method updateTabTriggers 
+     * @description Toggles active / inactive tab heads
+     * 
+     */
+    updateTabTriggers(activeTrigger) {
+        this.tabTriggers.forEach((trigger) => {
+            if (trigger === activeTrigger) {
+                trigger.setAttribute('data-is-active', 'true');
+            } else {
+                trigger.setAttribute('data-is-active', 'false');
+            }
+        });
+    }
+
+    /** 
+     * 
+     * @method updateTabContents 
+     * @description Toggles active / inactive tab content containers
+     * 
+     */
+    updateTabContents(activeTabContent) {
+        this.tabContents.forEach((tabContent) => {
+            if (tabContent === activeTabContent) {
+                tabContent.setAttribute('data-is-active', 'true');
+            } else {
+                tabContent.setAttribute('data-is-active', 'false');
+            }
+        });
+    }
+
+    /** 
+     * 
+     * @method throwEvent 
+     * @description Invokes a custom event with details
+     * 
+     */
+    throwEvent(eventName, payload) {
+        // create custom event
+        const customEvent = new CustomEvent(eventName, {
+            detail: {
+                payload
+            }
+        });
+
+        // throw the event ( usage: document.addEventListener('custom_eventName', (evt) => { console.log('status changed', evt.detail); }); )
+        document.dispatchEvent(customEvent);
+    }
+}
+
+/**
+ * 
  * @class ScrollableTabs
  * @author Abirlal Maiti <abirlal.maiti@gmail.com>
  * 
@@ -248,4 +369,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
     // Instantiate the dropdownTabs class to apply the functionality
     const dropdownTabs = new DropdownTabs();
+    
+    // Initialize the tabs class for all elements with data-tab-main-wrapper (if needed)
+	/* document.querySelectorAll('[data-tab-main-wrapper]').forEach((element) => {
+		const dashTab = new DashTabs(element);
+	}); */
 });
